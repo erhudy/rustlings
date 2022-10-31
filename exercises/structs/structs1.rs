1876ec540e18eaef426c8a9d16238a9e6439d0b5
Edmund Rhudy <erhudy@users.noreply.github.com>
2022-10-31 19:42:30 -0400

    changes

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -1,16 +1,15 @@
 // structs1.rs
 // Address all the TODOs to make the tests pass!
-// Execute `rustlings hint structs1` or use the `hint` watch subcommand for a hint.
 
-struct ColorClassicStruct {
-    name: String,
-    hex: String,
+struct ColorClassicStruct<'a> {
+    name: &'a str,
+    hex: &'a str
 }
 
-struct ColorTupleStruct(String, String);
+struct ColorTupleStruct<'a>(&'a str, &'a str);
 
 #[derive(Debug)]
-struct UnitLikeStruct;
+struct UnitStruct;
 
 #[cfg(test)]
 mod tests {
@@ -18,30 +17,28 @@ mod tests {
 
     #[test]
     fn classic_c_structs() {
-        let green = ColorClassicStruct {
-            name: "green".to_string(),
-            hex: "#00FF00".to_string(),
-        };
+        // TODO: Instantiate a classic c struct!
+        let green = ColorClassicStruct{ hex: "#00FF00", name: "green"};
 
-        assert_eq!(green.red, 0);
-        assert_eq!(green.green, 255);
-        assert_eq!(green.blue, 0);
+        assert_eq!(green.name, "green");
+        assert_eq!(green.hex, "#00FF00");
     }
 
     #[test]
     fn tuple_structs() {
-        let green = ColorTupleStruct("green".to_string(), "#00FF00".to_string());
+        // TODO: Instantiate a tuple struct!
+        let green = ColorTupleStruct("green", "#00FF00");
 
-        assert_eq!(green.0, 0);
-        assert_eq!(green.1, 255);
-        assert_eq!(green.2, 0);
+        assert_eq!(green.0, "green");
+        assert_eq!(green.1, "#00FF00");
     }
 
     #[test]
     fn unit_structs() {
-        let unit_struct = UnitStruct{};
+        // TODO: Instantiate a unit struct!
+        let unit_struct = UnitStruct;
         let message = format!("{:?}s are fun!", unit_struct);
 
-        assert_eq!(message, "UnitLikeStructs are fun!");
+        assert_eq!(message, "UnitStructs are fun!");
     }
 }
